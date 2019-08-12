64d6bca46746410653b43aff9a633ce67e96be51
mouc <chzmyf@gmail.com>
2019-08-12 17:50:39 +0800

    优化grid筛选、默认不显示图标、 快捷添加、快捷键操作

--- app/Admin/Controllers/SiteController.php
+++ app/Admin/Controllers/SiteController.php
@@ -4,7 +4,7 @@ namespace App\Admin\Controllers;
 
 use App\Admin\Extensions\Tools\BatchRestore;
 use App\Category;
-use App\Http\Controllers\Controller;
+use Encore\Admin\Controllers\AdminController;
 use App\Site;
 use Encore\Admin\Controllers\HasResourceActions;
 use Encore\Admin\Form;
@@ -12,64 +12,14 @@ use Encore\Admin\Grid;
 use Encore\Admin\Layout\Content;
 use Encore\Admin\Show;
 
-class SiteController extends Controller
+class SiteController extends AdminController
 {
-    use HasResourceActions;
-
     /**
-     * Index interface.
+     * Title for current resource.
      *
-     * @param Content $content
-     * @return Content
+     * @var string
      */
-    public function index(Content $content)
-    {
-        return $content
-            ->header('网站管理')
-            ->body($this->grid());
-    }
-
-    /**
-     * Show interface.
-     *
-     * @param mixed $id
-     * @param Content $content
-     * @return Content
-     */
-    public function show($id, Content $content)
-    {
-        return $content
-            ->header('Detail')
-            ->description('description')
-            ->body($this->detail($id));
-    }
-
-    /**
-     * Edit interface.
-     *
-     * @param mixed $id
-     * @param Content $content
-     * @return Content
-     */
-    public function edit($id, Content $content)
-    {
-        return $content
-            ->header('编辑网站')
-            ->body($this->form()->edit($id));
-    }
-
-    /**
-     * Create interface.
-     *
-     * @param Content $content
-     * @return Content
-     */
-    public function create(Content $content)
-    {
-        return $content
-            ->header('新增网站')
-            ->body($this->form());
-    }
+    protected $title = '网站';
 
     /**
      * Make a grid builder.
@@ -80,38 +30,57 @@ class SiteController extends Controller
     {
         $grid = new Grid(new Site);
 
+        $grid->quickSearch('title', 'url');
+
+        $grid->quickCreate(function (Grid\Tools\QuickCreate $create) {
+            $create->select('category_id', '分类')
+            ->options(Category::selectOptions())
+            ->rules('required');
+            $create->text('title', '标题')
+            ->attribute('autocomplete', 'off')
+            ->rules('required|max:50');
+            $create->text('url', '地址')
+            ->attribute('autocomplete', 'off')
+            ->rules('required|max:250');
+        });
+
+        $grid->model()->orderBy('id', 'desc');
         $grid->id('ID');
         $grid->category()->title('分类');
-        $grid->title('标题');
-        $grid->thumb('图标')->gallery(['width' => 25, 'height' => 25]);
+        $grid->title('标题')->copyable();
+        $grid->thumb('图标')->gallery(['width' => 25, 'height' => 25])->hide();
         $grid->describe('描述')->limit(40);
-        $grid->url('地址');
+        $grid->url('地址')->link();//->copyable()
+        ;
 
         $isTrash = request('_scope_') === 'trashed';
 
         if ($isTrash) {
             $grid->tools(function ($tools) {
-
                 $tools->batch(function (Grid\Tools\BatchActions $batch) {
                     $batch->disableDelete();
                     $batch->add('还原', new BatchRestore());
                 });
-
             });
         }
 
+
         $grid->filter(function ($filter) {
             $filter->disableIdFilter();
-
-            $categories = Category::query()->pluck('title', 'id');
-            $filter->equal('category_id', '分类')->select($categories);
-
-            $filter->like('title', '标题');
+            $filter->column(1/2, function ($filter) {
+                $filter->equal('category_id', '分类')->select(Category::query()->pluck('title', 'id'));
+            });
+            $filter->column(1/2, function ($filter) {
+                $filter->like('title', '标题');
+            });
 
             $filter->scope('trashed', '回收站')->onlyTrashed();
+            $filter->scope('ssl', 'HTTPS')->where('url', 'like', 'https%');
+            $filter->scope('nossl', 'HTTP')->where('url', 'not like', 'https%');
         });
 
         $grid->disableExport();
+        $grid->enableHotKeys();
 
         return $grid;
     }
@@ -155,13 +124,14 @@ class SiteController extends Controller
             ->rules('required|max:50');
         $form->image('thumb', '图标')
             ->crop(120, 120)
-            ->uniqueName()
-            ->rules('required');
+            ->uniqueName();
+        //->rules('required');
         $form->text('describe', '描述')
             ->attribute('autocomplete', 'off')
-            ->rules('required|max:300');
+            ->rules('max:300');
         $form->url('url', '地址')
             ->attribute('autocomplete', 'off')
+            ->help('请输入 http:// 或 https:// 开头的网址')
             ->rules('required|max:250');
 
         $form->footer(function ($footer) {
